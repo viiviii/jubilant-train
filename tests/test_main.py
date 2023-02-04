fcdc75debf550ae22f9932090de917584e1c2644
viiviii <viiiix.viiviii@gmail.com>
2023-02-04 16:36:00 +0900

    :sparkles: feat: 계정의 비밀번호는 마스킹되어 보여진다

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,6 @@
 import pytest
 
+from lotto.account import Account
 from lotto.lotto import go_lotto, total_price
 from main import login, LottoError, buy
 
@@ -11,15 +12,16 @@ def test_login_success():
 
 # todo: 테스트 lotto로 이동하거나 제거
 def test_buy(account):
-    login(*account)  # todo
+    login(account)  # todo
     go_lotto()
     buy(amount=5)
     assert total_price() == 5 * 1000
 
 
-def test_login_failure_when_wrong_login_info():
+def test_login_failure_when_invalid_account():
     with pytest.raises(LottoError, match='[로그인 실패] *'):
-        login('wrong125id2541', 'wrong@password')
+        invalid_account = Account('invalid125id2541', 'invalid@password')
+        login(invalid_account)
 
 
 def test_buy_failure_when_not_logged_in():
