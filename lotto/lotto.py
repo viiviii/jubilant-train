c0577cd26afc594d3a1e599661c642a5bf815599
viiviii <viiiix.viiviii@gmail.com>
2023-02-22 22:16:35 +0900

    ♻️ refactor: 기능들을 클래스로 구분하고 파일을 나눈다 (#20)
    * :truck: 최근 당첨 결과 확인 기능을 하나의 파일로 추출한다
    
    * :white_check_mark: 테스트를 추가하고 실행되도록 구현한다
    
    * :recycle: 기존 코드를 Lotto 구현체로 변경한다
    
    * :recycle: 테스트 코드와 동일하게 page 클래스로 구조를 나눈다
    
    * :recycle: refactor: main.py를 제거하고 클래스명을 변경한다

--- lotto/lotto.py
+++ lotto/lotto.py
@@ -1,131 +1,39 @@
+from abc import ABCMeta, abstractmethod
 from datetime import date
-from itertools import zip_longest
-from typing import List, Optional
-
-from selenium import webdriver
-from selenium.common import WebDriverException, NoSuchElementException
-from selenium.webdriver.common.alert import Alert
-from selenium.webdriver.common.by import By
-from selenium.webdriver.remote.webelement import WebElement
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.select import Select
-from selenium.webdriver.support.wait import WebDriverWait
-
-_driver = webdriver.Chrome()
-
-# todo
-_base_url = 'https://dhlottery.co.kr'
-_login_url = f'{_base_url}/user.do?method=login'
+from typing import Any
 
 
-def go_login() -> None:
-    _driver.get(_login_url)
+class Account(object):
+    def __init__(self, account_id: str, account_password: str) -> None:
+        self.id = account_id
+        self.password = account_password
+
+    def __str__(self) -> str:
+        def mask(string) -> str:
+            return '*' * len(string)
+
+        return f'id={self.id}, ' \
+               f'password={mask(self.password) if self.password else self.password}'
+
+    def __repr__(self) -> str:
+        return f'Account({self})'
 
 
-def login_input_boxs() -> List[WebElement]:
-    assert _driver.current_url == _login_url
-    return _driver.find_elements(By.CSS_SELECTOR, '#article .form input')
+class Lotto(metaclass=ABCMeta):
+
+    @abstractmethod
+    def login(self, account: Account) -> None:
+        pass
+
+    @abstractmethod
+    def buy(self, amount: int) -> int:
+        pass
+
+    @abstractmethod
+    def result(self, start: date, end: date) -> dict[str, Any]:
+        pass
 
 
-def login_button() -> WebElement:
-    assert _driver.current_url == _login_url
-    return _driver.find_element(By.CSS_SELECTOR, '#article .form a')
-
-
-def go_lotto() -> None:
-    _driver.get('https://ol.dhlottery.co.kr/olotto/game/game645.do')
-
-
-# todo
-def alert() -> Optional[Alert]:
-    try:
-        WebDriverWait(_driver, 5).until(expected_conditions.alert_is_present())
-        return _driver.switch_to.alert
-    except WebDriverException:
-        return None
-
-
-def layer_popup() -> WebElement:
-    return _driver.find_element(By.CSS_SELECTOR, '#popupLayerAlert')
-
-
-def amount_select() -> Select:
-    return Select(_driver.find_element(By.CSS_SELECTOR, '#amoundApply'))
-
-
-def auto_checkbox() -> WebElement:
-    return _driver.find_element(By.CSS_SELECTOR, 'label[for="checkAutoSelect"]')
-
-
-def apply_button() -> WebElement:
-    return _driver.find_element(By.CSS_SELECTOR, '#btnSelectNum')
-
-
-def buy_button() -> WebElement:
-    return _driver.find_element(By.CSS_SELECTOR, '#btnBuy')
-
-
-# todo
-def confirm_button() -> WebElement:
-    return _driver.find_element(By.CSS_SELECTOR, '#popupLayerConfirm input[value="확인"]')
-
-
-def total_price() -> int:
-    element = _driver.find_element(By.CSS_SELECTOR, '#payAmt')
-    return int(element.text.replace(',', ''))
-
-
-# todo
-def title() -> str:
-    return _driver.title
-
-
-def go_my_buy(start_date: date, end_date: date) -> None:
-    fmt = '%Y%m%d'  # YYYYMMDD
-    _driver.get(f'{_base_url}/myPage.do?method=lottoBuyList'
-                f'&searchStartDate={start_date.strftime(fmt)}'
-                f'&searchEndDate={end_date.strftime(fmt)}'
-                f'&lottoId=&nowPage=1')  # 로또 게임 아이디=LO40
-
-
-def buy_results() -> List[dict[str, str]]:
-    headers = [th.text for th in _driver.find_elements(By.CSS_SELECTOR, '.tbl_data thead th')]
-    rows = _driver.find_elements(By.CSS_SELECTOR, '.tbl_data tbody tr')
-
-    buys = []
-    for row in rows:
-        tds = row.find_elements(By.TAG_NAME, 'td')
-        buys.append(dict(zip_longest(headers, [td.text for td in tds], fillvalue='')))
-
-    return buys
-
-
-def total_buy_result(buys: List[dict[str, str]]) -> dict[str, int]:
-    amount = [extract_amount(lottery['당첨금']) for lottery in buys]
-    count = [int(lottery['구입매수'] or 0) for lottery in buys]
-    unpick = [int(lottery['구입매수'] or 0) for lottery in buys if lottery['당첨결과'] == '미추첨']
-
-    return {
-        '총 당첨금': sum(amount),
-        '총 구입매수': sum(count),
-        '미추첨': sum(unpick),
-    }
-
-
-def extract_amount(value: str) -> int:
-    if not value or value == '-':
-        return 0
-
-    return int(''.join(filter(str.isdigit, value)))
-
-
-# todo
-def no_data_message() -> Optional[str]:
-    """메세지 종류:
-        로그인 후 이용이 가능합니다.
-        조회 결과가 없습니다.
-    """
-    try:
-        return _driver.find_element(By.CSS_SELECTOR, '.nodata').text
-    except NoSuchElementException:
-        return None
+class LottoError(Exception):
+    def __init__(self, reason: str, detail: str):
+        super().__init__(f'[{reason}] {detail}')
---
+++ tests/test_check_latest_lotto_result.py
@@ -0,0 +1,63 @@
+from datetime import date
+from typing import Any
+
+from check_latest_lotto_result import to_content, last_sunday, check_latest_lotto_result, DateRange, to_title
+from lotto.lotto import Account
+from lotto.lotto import Lotto
+from sends.send import Send, SendResult
+
+
+def test_check_latest_lotto_result():
+    result = check_latest_lotto_result(
+        account=Account(account_id='id', account_password='password'),
+        lotto=StubLotto(),
+        send=StubSend(),
+        search_dates=DateRange(date(2023, 2, 12), date(2023, 2, 18))
+    )
+
+    assert result.title == '🎊 로또6/45 1054회(2023-02-11)'
+    assert result.content == ('💰 총 당첨금: 1,234,567원\n'
+                              '📅 조회기간: 23-02-12 ~ 23-02-18')
+
+
+class StubLotto(Lotto):
+
+    def login(self, account: Account) -> None:
+        pass
+
+    def buy(self, amount: int) -> int:
+        return amount * 1000
+
+    def result(self, start: date, end: date) -> dict[str, Any]:
+        return {'복권명': '로또6/45', '회차': 1054, '추첨일': date(2023, 2, 11),
+                '총 당첨금': 1234567,
+                '조회 시작일': start, '조회 종료일': end}
+
+
+class StubSend(Send):
+    def __call__(self, title: str, content: str) -> SendResult:
+        return SendResult(title=title, content=content)
+
+
+def test_last_sunday():
+    sunday = date(2023, 1, 1)
+    monday = date(2023, 1, 2)
+
+    assert last_sunday(today=monday) == sunday
+    assert last_sunday(today=sunday) == sunday
+
+
+def test_to_title():
+    actual = to_title(name='로또6/45', rounds='1054', draw_date=date(2023, 2, 11))
+
+    assert actual == '🎊 로또6/45 1054회(2023-02-11)'
+
+
+def test_to_content():
+    actual = to_content(
+        total_amount=0,
+        search_dates=DateRange(date(2020, 1, 1), date(2021, 11, 28)))
+
+    assert actual == (
+        '💰 총 당첨금: 0원\n'
+        '📅 조회기간: 20-01-01 ~ 21-11-28')
