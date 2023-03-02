3f69393c8f63849e78e65efe412da4ffc1e44406
viiviii <viiiix.viiviii@gmail.com>
2023-03-02 15:54:03 +0900

    :recycle: refactor: 객체 로직에 관계 없이 페이지 속성이 테스트 되도록 변경한다

--- tests/test_lotto_site_page.py
+++ tests/test_lotto_site_page.py
@@ -1,5 +1,3 @@
-from datetime import date
-
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -12,6 +10,16 @@ def driver():
     _driver = webdriver.Chrome()
     yield _driver
     _driver.quit()
+
+
+@pytest.fixture(scope='class')
+def load(driver, page):
+    driver.get(page.URL)
+
+
+@pytest.fixture(scope='class')
+def by(page):
+    return page.By
 
 
 @pytest.fixture(scope='class')
@@ -34,22 +42,22 @@ def login(driver, account):
 class TestLoginPageElements:
 
     @pytest.fixture(scope='class')
-    def page(self, driver):
-        return LoginPage(driver)
+    def page(self):
+        return LoginPage
 
     @pytest.fixture(scope='class', autouse=True)
-    def setup(self, page):
-        page.go()
+    def setup(self, load):
+        pass
 
-    def test_account_inputs(self, page, find_elements):
-        elements = find_elements(page.By.ACCOUNT_INPUTS)
+    def test_account_inputs(self, by, find_elements):
+        elements = find_elements(by.ACCOUNT_INPUTS)
 
         assert len(elements) == 2
         assert elements[0].accessible_name == '아이디'
         assert elements[1].accessible_name == '비밀번호'
 
-    def test_login_button(self, page, find_element):
-        element = find_element(page.By.LOGIN_BUTTON)
+    def test_login_button(self, by, find_element):
+        element = find_element(by.LOGIN_BUTTON)
 
         assert element
         assert element.accessible_name == '로그인'
@@ -58,44 +66,44 @@ class TestLoginPageElements:
 class TestLottoPageElements:
 
     @pytest.fixture(scope='class')
-    def page(self, driver):
-        return LottoPage(driver)
+    def page(self):
+        return LottoPage
 
     @pytest.fixture(scope='class', autouse=True)
-    def setup(self, login, page):
-        page.go()
+    def setup(self, login, load):
+        pass
 
-    def test_auto_checkbox(self, page, find_element):
-        assert find_element(page.By.AUTO_CHECKBOX)
+    def test_auto_checkbox(self, by, find_element):
+        assert find_element(by.AUTO_CHECKBOX)
 
-    def test_quantity_select(self, page, find_element):
-        assert find_element(page.By.QUANTITY_SELECT)
+    def test_quantity_select(self, by, find_element):
+        assert find_element(by.QUANTITY_SELECT)
 
-    def test_apply_button(self, page, find_element):
-        assert find_element(page.By.APPLY_BUTTON)
+    def test_apply_button(self, by, find_element):
+        assert find_element(by.APPLY_BUTTON)
 
-    def test_buy_button(self, page, find_element):
-        assert find_element(page.By.BUY_BUTTON)
+    def test_buy_button(self, by, find_element):
+        assert find_element(by.BUY_BUTTON)
 
-    def test_buy_confirm_button(self, page, find_element):
-        assert find_element(page.By.BUY_CONFIRM_BUTTON)
+    def test_buy_confirm_button(self, by, find_element):
+        assert find_element(by.BUY_CONFIRM_BUTTON)
 
 
 class TestMyBuyPageElements:
 
     @pytest.fixture(scope='class')
-    def page(self, driver):
-        return MyBuyPage(driver)
+    def page(self):
+        return MyBuyPage
 
     @pytest.fixture(scope='class', autouse=True)
-    def setup(self, login, page):
-        page.go(dates=(date.today(), date.today()))
+    def setup(self, login, load):
+        pass
 
-    def test_history_table(self, page, find_element):
-        assert find_element(page.By.HISTORY_TABLE)
+    def test_history_table(self, by, find_element):
+        assert find_element(by.HISTORY_TABLE)
 
-    def test_history_table_headers(self, page, find_element):
-        headers = Table.from_element(find_element(page.By.HISTORY_TABLE)).headers
+    def test_history_table_headers(self, by, find_element):
+        headers = Table.from_element(find_element(by.HISTORY_TABLE)).headers
         assert '구입일자' in headers
         assert '복권명' in headers
         assert '회차' in headers
