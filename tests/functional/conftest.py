10001078e6898ca96210ce7c4c47593894a89714
viiviii <viiiix.viiviii@gmail.com>
2023-03-06 21:58:55 +0900

    :fire: remove: 불필요해진 local keyring 설정을 제거한다

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -2,12 +2,10 @@ from datetime import date, datetime
 
 import pytest
 import requests
-from _pytest.config.argparsing import Parser
 from selenium import webdriver
 
 import lotto.account
 import sends.auth
-from lotto.account import ID_ARGUMENT_OPTIONS, ID_ARGUMENT_NAME
 
 
 @pytest.fixture(scope='class')
@@ -25,14 +23,6 @@ def account() -> lotto.account.Account:
 @pytest.fixture(scope='session')
 def auth() -> sends.auth.Auth:
     return sends.auth.from_env()
-
-
-def pytest_addoption(parser):
-    _add_id_option_for_passing_argument_to_pytest(parser)
-
-
-def _add_id_option_for_passing_argument_to_pytest(parser: Parser):
-    parser.addoption(ID_ARGUMENT_NAME, **ID_ARGUMENT_OPTIONS)
 
 
 @pytest.fixture(scope='session')
