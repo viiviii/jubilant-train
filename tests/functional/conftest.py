d4c098c4d3a817e8ab1066a686d3f5b9f939f50f
viiviii <viiiix.viiviii@gmail.com>
2023-03-06 16:28:14 +0900

    ♻️ refactor: send test를 실제로 테스트한다 (#23)
    * :sparkles: feat: 이슈 생성 옵션에 라벨을 추가한다
    
    * :white_check_mark: test: send github issue 테스트는 실제 구현을 테스트한다
    
    * :heavy_minus_sign: config: 필요없어진 requests-mock을 제거한다

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -1,8 +1,12 @@
+from datetime import date, datetime
+
 import pytest
+import requests
 from _pytest.config.argparsing import Parser
 from selenium import webdriver
 
 from lotto.account import Account, fetch_account, ID_ARGUMENT_OPTIONS, ID_ARGUMENT_NAME
+from lotto.secret import Secret
 
 
 @pytest.fixture(scope='class')
@@ -23,3 +27,65 @@ def pytest_addoption(parser):
 
 def _add_id_option_for_passing_argument_to_pytest(parser: Parser):
     parser.addoption(ID_ARGUMENT_NAME, **ID_ARGUMENT_OPTIONS)
+
+
+@pytest.fixture(scope='session')
+def token():
+    # todo: env에서 가져오기
+    return Secret('token')
+
+
+@pytest.fixture(scope='session')
+def repository():
+    # todo: env에서 가져오기
+    return 'viiviii/something'
+
+
+@pytest.fixture(scope='session')
+def labels():
+    return ['for-testing']
+
+
+def close_testing_issues(token, repository, labels, since):
+    issues = issues_created_by_tests(token=token, repository=repository, labels=labels, since=since)
+
+    for issue in issues:
+        close_issue(token=token, repository=repository, number=issue['number'])
+
+
+def issues_created_by_tests(token, repository, labels, since: datetime = date.today()):
+    response = requests.get(
+        f'https://api.github.com/repos/{repository}/issues',
+        headers={
+            'Accept': 'application/vnd.github+json',
+            'Authorization': f'Bearer {token.value}',
+            'X-GitHub-Api-Version': '2022-11-28',
+        },
+        params={
+            'state': 'open',
+            'creator': repository.split('/')[0],
+            'labels': labels,
+            'since': since.isoformat()
+        },
+    )
+
+    assert response.ok, f'이슈 조회 실패=[{response.reason}] {response.text}'
+
+    return response.json()
+
+
+def close_issue(token, repository, number):
+    response = requests.patch(
+        f'https://api.github.com/repos/{repository}/issues/{number}',
+        headers={
+            'Accept': 'application/vnd.github+json',
+            'Authorization': f'Bearer {token.value}',
+            'X-GitHub-Api-Version': '2022-11-28',
+        },
+        json={
+            'state': 'closed',
+            'state_reason': 'completed',
+        }
+    )
+
+    assert response.ok, f'이슈 종료 실패=[{response.reason}] {response.text}'
