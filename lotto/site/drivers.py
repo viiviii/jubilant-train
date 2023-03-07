f4e461e46692da5541f72c19ff4859a52bb5c6c7
viiviii <viiiix.viiviii@gmail.com>
2023-03-07 16:06:06 +0900

    :bug: bug: 로컬에서 실행 시 발생하는 ElementClickInterceptedException를 해결한다

--- lotto/site/drivers.py
+++ lotto/site/drivers.py
@@ -4,7 +4,8 @@ from selenium import webdriver
 def headless_chrome() -> webdriver:
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
-    
+    options.add_argument('--window-size=1920,1080')
+
     return chrome(options=options)
 
 
