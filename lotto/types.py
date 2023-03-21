5a63300fb99c54a78e5f85867638cc0f0a61bfb1
viiviii <viiiix.viiviii@gmail.com>
2023-03-21 21:19:00 +0900

    :fire: remove: 불필요해진 Table zip() 메서드를 제거한다

--- lotto/types.py
+++ lotto/types.py
@@ -1,6 +1,5 @@
 from dataclasses import dataclass
 from datetime import date
-from itertools import zip_longest
 from typing import NamedTuple, List
 
 DateRange = NamedTuple('DateRange', [('start', date), ('end', date)])
@@ -13,7 +12,3 @@ class Table:
 
     def values(self, key: str) -> List[str]:
         return [row[self.headers.index(key)] for row in self.rows]
-
-    def zip(self):
-        return [dict(zip_longest(self.headers, cells, fillvalue=''))
-                for cells in self.rows]
--- tests/unit/lotto/test_types.py
+++ tests/unit/lotto/test_types.py
@@ -25,31 +25,6 @@ class TestTable:
 
         assert actual == ['박덕배', '장평수']
 
-    def test_zip(self, create_table):
-        headers = ['이름', '나이']
-        rows = [['박덕배', '22'], ['장평수', '7']]
-
-        actual = create_table(headers=headers, rows=rows).zip()
-
-        assert len(actual) == 2
-        assert actual[0] == {'이름': '박덕배', '나이': '22'}
-        assert actual[1] == {'이름': '장평수', '나이': '7'}
-
-    def test_zip_when_empty_rows(self, create_table):
-        empty_arr = []
-
-        actual = create_table(rows=empty_arr).zip()
-
-        assert actual == []
-
-    def test_zip_when_nested_empty_rows(self, create_table):
-        nested_empty_arr = [[]]
-
-        actual = create_table(rows=nested_empty_arr).zip()
-
-        assert len(actual) == 1
-        assert actual[0] == {'이름': '', '나이': ''}
-
     def test_as_dict(self, create_table):
         table = create_table(
             headers=['이름', '나이'],
@@ -58,5 +33,7 @@ class TestTable:
 
         actual = asdict(table)
 
-        assert actual == {'headers': ['이름', '나이'],
-                          'rows': [['박덕배', '22'], ['장평수', '7']]}
+        assert actual == {
+            'headers': ['이름', '나이'],
+            'rows': [['박덕배', '22'], ['장평수', '7']]
+        }
